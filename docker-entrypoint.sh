c191d018e50f69b77e09efa8e990252e04b9722e
Kristián Feldsam <feldsam@gmail.com>
2018-10-10 16:58:10 +0200

    Customized docker deployment
    Signed-off-by: Kristián Feldsam <feldsam@gmail.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,6 +5,10 @@ if [ ! -f "/app/config/production.toml" ] ; then
     echo "No production.toml, copying from docker-production.toml.tmpl"
     cp /app/config/docker-production.toml.tmpl /app/config/production.toml
 fi
+if [ ! -f "/app/config/local.js" ] ; then
+    echo "No local.js, copying from docker-local.js.tmpl"
+    cp /app/config/docker-local.js.tmpl /app/config/local.js
+fi
 if [ ! -f "/app/workers/reports/config/production.toml" ] ; then 
     echo "No production.toml for reports"
     if [ -f "/app/config/production.toml" ] ; then 
@@ -13,6 +17,18 @@ if [ ! -f "/app/workers/reports/config/production.toml" ] ; then
     else
         echo "copying config/docker-production.toml.tmpl to reports config directory as production.toml"
         cp /app/config/docker-production.toml.tmpl /app/workers/reports/config/production.toml
+        echo "copying config/docker-local.js.tmpl to reports config directory as local.js"
+        cp /app/config/docker-local.js.tmpl /app/workers/reports/config/local.js
+    fi
+fi
+if [ ! -f "/app/workers/reports/config/local.js" ] ; then
+    echo "No local.js for reports"
+    if [ -f "/app/config/local.js" ] ; then
+        echo "copying config/local.js to reports config directory"
+        cp /app/config/local.js /app/workers/reports/config/local.js
+    else
+        echo "copying config/docker-local.js.tmpl to reports config directory as local.js"
+        cp /app/config/docker-local.js.tmpl /app/workers/reports/config/local.js
     fi
 fi
 exec "$@"
